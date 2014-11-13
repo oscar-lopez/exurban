9d78ead24c9faf6353a2458b9e0e3dca6339fa94
arpunk <arpunk@cryptolab.net>
2014-11-12 22:32:18 -0500

    Use timeout from envs and return a list with the error key when HTTPoison fails

--- lib/exurban/utils.ex
+++ lib/exurban/utils.ex
@@ -1,19 +1,21 @@
 defmodule ExUrban.Utils do
   use Jazz
 
+  @timeout Application.get_env(:urban, :ua_timeout)
+
   def query(method, status, endpoint, object \\ %{}) do
-    auth = [hackney: ExUrban.make_auth]
+    opts = [hackney: ExUrban.make_auth, timeout: @timeout]
     res = case method do
-      :get    -> ExUrban.get endpoint, ExUrban.post_headers, auth
-      :post   -> ExUrban.post endpoint, JSON.encode!(object), ExUrban.post_headers, auth
-      :put    -> ExUrban.put endpoint, JSON.encode!(object), ExUrban.post_headers, auth
-      :delete -> ExUrban.delete endpoint, ExUrban.post_headers, auth
+      :get    -> ExUrban.get endpoint, ExUrban.post_headers, opts
+      :post   -> ExUrban.post endpoint, JSON.encode!(object), ExUrban.post_headers, opts
+      :put    -> ExUrban.put endpoint, JSON.encode!(object), ExUrban.post_headers, opts
+      :delete -> ExUrban.delete endpoint, ExUrban.post_headers, opts
     end
 
     case res do
       {:ok, %HTTPoison.Response{status_code: ^status, body: body}} -> {:ok, body}
       {:ok, %HTTPoison.Response{status_code: _, body: body}}       -> {:fail, body}
-      {:error, %HTTPoison.Error{reason: reason}}                   -> {:fail, reason}
+      {:error, %HTTPoison.Error{reason: reason}}                   -> {:fail, [error: reason]}
     end
 
   end
