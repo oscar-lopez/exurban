bee5050fe6461348fdff89d097d33d9276cac1c8
olopez <olopez@tappsi.co>
2014-11-24 19:12:41 -0500

    modified logger

--- lib/exurban/utils.ex
+++ lib/exurban/utils.ex
@@ -5,18 +5,32 @@ defmodule ExUrban.Utils do
   @timeout Application.get_env(:urban, :ua_timeout)
 
   def query(method, status, endpoint, object \\ %{}) do
-    opts = [{:hackney, ExUrban.make_auth}, {:timeout, @timeout}]
 
-    # TODO: delete this logger
-    Logger.error("--------------------------------------------------------------")
-    Logger.error("message  : #{JSON.encode!(object)}")
-    Logger.error("--------------------------------------------------------------")
+    opts = [{:hackney, ExUrban.make_auth}, {:timeout, @timeout}]
 
     res = case method do
       :get    -> ExUrban.get endpoint, ExUrban.post_headers, opts
       :post   -> ExUrban.post endpoint, JSON.encode!(object), ExUrban.post_headers, opts
       :put    -> ExUrban.put endpoint, JSON.encode!(object), ExUrban.post_headers, opts
       :delete -> ExUrban.delete endpoint, ExUrban.post_headers, opts
+    end
+
+    # TODO: delete this logger
+    case res do
+      {:ok, %HTTPoison.Response{status_code: ^status, body: _}} -> :ok
+      {:ok, %HTTPoison.Response{status_code: code, body: body}}       -> 
+        Logger.error("-------------------------------------")
+        Logger.error("status code : #{code}")
+        Logger.error("body        : #{body}")
+        Logger.error("message     : #{JSON.encode!(object)}")
+        Logger.error("-------------------------------------")
+        :nok
+      {:error, %HTTPoison.Error{reason: reason}}                   ->
+        Logger.error("-------------------------------------")
+        Logger.error("reason      : #{reason}")
+        Logger.error("message     : #{JSON.encode!(object)}")
+        Logger.error("-------------------------------------")
+        :nok
     end
 
     case res do
